7c914cbfc098ed8d3f3eacab6c23369608c1b9a5
Brandy Medina <brandy.medina4640@alumnos.udg.mx>
2020-09-19 19:45:19 -0500

    se muestra el  menu

---
+++ diagnostico.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main(){
+ 	char op;
+ 	do
+ 	{
+ 		printf("1) Capturar enteros\n");
+ 		printf("2) Mostrar cadena n veces \n");
+ 		printf("3) Agregar personajes\n");
+ 		printf("4) Mostrar personajes\n");
+ 		printf("0) Salir \n");
+ 		scanf("%c",&op);
+ 		fflush(stdin);
+ 	
+	 } while(op!=0);
+ }
