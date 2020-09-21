c34ecb0385abd323b1a490ad4f8a868d654d88d8
Brandy Medina <brandy.medina4640@alumnos.udg.mx>
2020-09-20 21:04:59 -0500

    Se ingresan dos numeros y se da el resultado de la suma

---
+++ mundo.py
@@ -0,0 +1,7 @@
+
+print("Hola mundo \n")
+print("Escribe 2 numeros para realizar un suma\n")
+num1= int (input())
+num2= int (input())
+resultado= num1 + num2 
+print("El resultado de la suma es = ",resultado)
